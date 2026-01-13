572335c71560a694f8bbab310c9e9ddb1a25b301
Pablo Landeta <landetap@gmail.com>
2026-01-12 19:51:08 -0500

    nuevo título

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hola():
-    return "<h1>¡Hola Pablo Andrés Landeta López!</h1>"
+    return "<h1>¡Hola Pablo Andrés Landeta López! Ejemplo de CI/CD hacia Docker en instancia EC2</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
