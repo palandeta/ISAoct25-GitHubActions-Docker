302dadc8e026353976705f013581911792458dd2
Pablo Landeta <landetap@gmail.com>
2026-01-12 19:28:18 -0500

    Cambio en el titulo

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hola():
-    return "<h1>¡Hola Pablo!</h1>"
+    return "<h1>¡Hola Pablo Landeta!</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-    
